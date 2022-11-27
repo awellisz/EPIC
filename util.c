6137c2c4c289f426d7c793ee112cbcb85b074e95
Antoni Wellisz <awwellisz@uchicago.edu>
2022-11-27 14:23:11 -0600

    Added util.c: get_time_ms()

---
+++ util.c
@@ -0,0 +1,12 @@
+// util.c
+
+#include <stdio.h>
+#include <sys/time.h> // Linux -- different for Windows, probably
+#include "defs.h"
+#include "functions.h"
+
+int get_time_ms() {
+    struct timeval t;
+    gettimeofday(&t, null);
+    return t.tv_sec*1000 + t.tv_usec/1000;
+}
