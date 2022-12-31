20443094017faf894b5d4ea7ee5dd25a34f4e3cb
Antoni Wellisz <awwellisz@uchicago.edu>
2022-12-31 02:43:08 +0100

    Added prototypes to header

--- pvtable.h
+++ pvtable.h
@@ -7,5 +7,7 @@
 void init_pvtable(pvtable_t *table);
 int probe_pvtable(const board_t *pos);
 void store_pv_move(const board_t *pos, const int move);
+void clear_pvtable(pvtable_t *table);
+int get_pv_line(const int depth, board_t *pos);
 
 #endif
